113bfb4a274a5343b868b80cb04d0e68d32f0691
Austin Tsang <aytsang14@gmail.com>
2019-06-25 11:39:42 -0700

    modified wrong config file lol

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,36 @@
+// module.exports = {
+//     apps: [{
+//         name: 'csc648-team6',
+//         script: './application/index.js',
+//         // cwd: "/home/ubuntu/server/application"
+//     }],
+//     deploy: {
+//         production: {
+//             user: 'ubuntu',
+//             host: 'ec2-13-52-181-2.us-west-1.compute.amazonaws.com',
+//             key: '~/.ssh/csc648-team6.pem',
+//             ref: 'origin/master',
+//             repo: 'git@github.com:CSC-648-SFSU/csc648-su19-Team06.git',
+//             path: '/home/ubuntu/server',
+//             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+//         }
+//     }
+// }
+
 module.exports = {
     apps: [{
         name: 'csc648-team6',
-        script: './application/index.js',
-        // cwd: "/home/ubuntu/server/application"
+        script: './index.js',
+        cwd: "/home/ubuntu/server/current/application"
     }],
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-13-52-181-2.us-west-1.compute.amazonaws.com',
-            key: '~/.ssh/csc648-team6.pem',
+            host: 'ec2-52-53-193-123.us-west-1.compute.amazonaws.com',
+            key: '~/.ssh/team6_csc648.pem',
             ref: 'origin/master',
             repo: 'git@github.com:CSC-648-SFSU/csc648-su19-Team06.git',
-            path: '/home/ubuntu/server',
+            path: '/home/ubuntu/server/',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
