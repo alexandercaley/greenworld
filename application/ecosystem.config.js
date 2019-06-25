6b3e12df84c4c3e251f2f57bb531719f58a43e51
Austin Tsang <aytsang14@gmail.com>
2019-06-24 20:31:17 -0700

    getting there

--- application/ecosystem.config.js
+++ application/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
             key: '~/.ssh/csc648-team6.pem',
             ref: 'origin/master',
             repo: 'git@github.com:CSC-648-SFSU/csc648-su19-Team06.git',
-            path: '/home/ubuntu/server/CSC-648-SFSU/application',
+            path: '/home/ubuntu/server',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
