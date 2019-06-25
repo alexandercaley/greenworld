a4ce4422121d139d11a703de85d28b774c1eb187
Austin Tsang <aytsang14@gmail.com>
2019-06-25 11:38:36 -0700

    re-created instance and changed config file a bit

--- application/ecosystem.config.js
+++ application/ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
     apps: [{
         name: 'csc648-team6',
         script: './index.js',
-        cwd: "/home/ubuntu/server/application"
+        cwd: "/home/ubuntu/server/current/application"
     }],
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-13-52-181-2.us-west-1.compute.amazonaws.com',
-            key: '~/.ssh/csc648-team6.pem',
+            host: 'ec2-52-53-193-123.us-west-1.compute.amazonaws.com',
+            key: '~/.ssh/team6_csc648.pem',
             ref: 'origin/master',
             repo: 'git@github.com:CSC-648-SFSU/csc648-su19-Team06.git',
-            path: '/home/ubuntu/server',
+            path: '/home/ubuntu/server/',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
