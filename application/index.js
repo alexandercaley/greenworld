d0508804cb5d49003765be59b4039e0dd854d151
Austin Tsang <aytsang14@gmail.com>
2019-06-25 13:54:15 -0700

    added everybody's html page. Those must be modified

--- application/index.js
+++ application/index.js
@@ -5,19 +5,7 @@ var path = require("path");
 app.use(express.static('public'))
 
 app.get('/austin', function(req, res) {
-    console.log("hi")
-    // res.render("./austin");
-    res.sendFile(path.join(__dirname+'/public/austin.html'));
+    res.sendFile(path.join(__dirname+'/public/aboutMe/austin.html'));
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'))
-
-// const path = require('path');
-// const app = express();
-
-// // express is not good for production static files, use cdn, or dedicated file server like ngnix, appache
-// app.use(express.static(path.join(__dirname, '..', 'build')));
-
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-// });
