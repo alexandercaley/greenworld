9b7be089660d6a87338e6ba626fc7d41b2978a2b
Austin Tsang <aytsang14@gmail.com>
2019-07-03 00:11:49 -0700

    used routing and created aboutus with individual endpoints for each student

--- application/index.js
+++ application/index.js
@@ -1,39 +1,16 @@
 const express = require('express')
 const app = express()
 var path = require("path");
+const aboutus = require('./api/aboutus.js');
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
+// hitting this route will bring us to aboutus page
+app.use('/aboutus', aboutus);
+
+// this will be the homepage.
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('/austin', function(req, res) {
-    res.sendFile(path.join(__dirname+'/public/aboutMe/austin.html'));
-});
-
-app.get('/han', function(req, res) {
-    res.sendFile(path.join(__dirname+'/public/aboutMe/han.html'));
-});
-
-app.get('/Gerry', function(req, res) {
-    res.sendFile(path.join(__dirname+'/public/aboutMe/gerry.html'));
-});
-
-app.get('/jesus', function(req, res) {
-	res.sendFile(path.join(__dirname+'/public/aboutMe/jesus.html'))
-})
-
-app.get('/audrey', function(req, res) {
-    res.sendFile(path.join(__dirname+'/public/aboutMe/audrey.html'))
-});
-
-app.get('/alexander', function(req, res) {
-	res.sendFile(path.join(__dirname+'/public/aboutMe/alexander.html'))
-})
-
-app.get('/raz', function(req, res) {
-    res.sendFile(path.join(__dirname+'/public/aboutMe/raz.html'))
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'))
