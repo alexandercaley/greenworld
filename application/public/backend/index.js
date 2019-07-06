f87838cbfa78ac176d785a039c1dcf1058a0591c
Alexander Caley <zanner11030@gmail.com>
2019-07-06 11:02:51 +0200

    basic infrastructure with no database architecture set up

--- application/public/backend/index.js
+++ application/public/backend/index.js
@@ -6,16 +6,51 @@ const app = express();
 
 const port = 4000;
 
+//Will need this code DO NOT DELETE
+
+// //Used to process post info
+// var bodyParser = require('body-parser');
+//
+// //x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+// //parse application/json
+// app.use(bodyParser.json());
+
+//Initializes the connection variable to sync with a MySQL database
+//Currently local database
+var connection = mysql.createConnection({
+	host: "localhost",
+
+	//Open port for DB
+	port: 3306,
+
+	// Your username
+	user: "root",
+
+	// Your password
+	password: "password",
+
+  //DB
+	database: "environmentaldb"
+});
+
 //route for accounts
 app.get('/api/accounts', (req, res) => {
+  console.log("\"/api/accounts\" route success!");
+  //test data
   const accounts = [
     {id: 1, accountname: 'account1', password: 'password1', usertype: 'admin'},
     {id: 2, accountname: 'account2', password: 'password2', usertype: 'user'},
     {id: 3, accountname: '', password: '', usertype: 'guest'}
   ]
-  console.log("\"/api/accounts\" route success!");
 
   res.send(accounts);
+});
+
+//post account data to database- IN PROGRESS
+app.post('/api/accounts', (req, res) => {
+
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
