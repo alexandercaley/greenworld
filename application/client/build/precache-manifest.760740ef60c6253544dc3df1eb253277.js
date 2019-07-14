5cf2da5401af22dc66818a725f737c2fa34aa7c6
Alexander Caley <zanner11030@gmail.com>
2019-07-14 15:32:41 -0700

    pull from master

---
+++ application/client/build/precache-manifest.760740ef60c6253544dc3df1eb253277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4408f03f1593a8392381",
+    "url": "/static/js/main.4617d60f.chunk.js"
+  },
+  {
+    "revision": "495cdf34c8fad78bfa33",
+    "url": "/static/js/2.916cd9a1.chunk.js"
+  },
+  {
+    "revision": "4408f03f1593a8392381",
+    "url": "/static/css/main.155e5f6a.chunk.css"
+  },
+  {
+    "revision": "4629f3b91e74f4125654788f4dea5e35",
+    "url": "/index.html"
+  }
+];
