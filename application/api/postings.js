b304d73b660eee4792c6e6d6915964c4f531ba5c
Alexander Caley <zanner11030@gmail.com>
2019-07-16 18:37:49 -0700

    working on post

--- application/api/postings.js
+++ application/api/postings.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql");
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
 
 let connection;
 if (!connection) {
@@ -12,10 +15,10 @@ if (!connection) {
     });
 
     connection.connect((err) => {
-      if (!err)
-      console.log('Connected to the MySQL server!');
-      else
-      console.log('Connection failed \n Error: ' + JSON.stringify(err, undefined, 2));
+        if (!err)
+            console.log('Connected to the MySQL server!');
+        else
+            console.log('Connection failed \n Error: ' + JSON.stringify(err, undefined, 2));
     });
 }
 
@@ -25,6 +28,26 @@ router.get('/', function (req, res) {
         if (err) throw err;
         res.json(result);
     });
+});
+
+router.post('/', function (req, res) {
+    console.log("POST!!!!")
+    var jsondata = req.body;
+    var values = [];
+
+    for (var i = 0; i < jsondata.length; i++) {
+        values.push([jsondata[i].id, jsondata[i].location, jsondata[i].postType, jsondata[i].postStatus, jsondata[i].picture]);
+    }
+
+    connection.query('INSERT INTO members (id, location, postType, postStatus, picture) VALUES ?', [values],
+        function (err, result) {
+            if (err) {
+                res.send('Error');
+            }
+            else {
+                res.send('Success');
+            }
+        });
 });
 
 module.exports = router;
