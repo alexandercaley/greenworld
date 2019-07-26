1f1922fdf2e2874e0b3225180866a3f7b44cd816
Austin Tsang <aytsang14@gmail.com>
2019-07-25 19:55:34 -0700

    deleted double reducers

--- application/client/src/index.js
+++ application/client/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
-
 
 // import redux
 import { createStore, combineReducers } from "redux";
