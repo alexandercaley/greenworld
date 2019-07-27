8520233d6e147ba075d6a9d139ec01766c9b6c43
HeyZeusV <valdesj419@gmail.com>
2019-07-27 15:17:37 -0700

    Database completed

--- application/database/csc648_team6_postings.sql
+++ application/database/csc648_team6_postings.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `VerticalPrototype` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `VerticalPrototype`;
 -- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: VerticalPrototype
+-- Host: 127.0.0.1    Database: csc648_team6
 -- ------------------------------------------------------
 -- Server version	5.7.26-0ubuntu0.18.04.1
 
@@ -25,24 +23,17 @@ DROP TABLE IF EXISTS `postings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `postings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `location` varchar(100) DEFAULT NULL,
-  `postType` varchar(20) DEFAULT NULL,
-  `postStatus` varchar(20) DEFAULT NULL,
-  `picture` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `users_registration_id` int(11) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `picture` text NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `date_witnessed` varchar(30) NOT NULL,
+  `topic` varchar(20) NOT NULL,
+  KEY `users_registration_id` (`users_registration_id`),
+  CONSTRAINT `users_registration_id` FOREIGN KEY (`users_registration_id`) REFERENCES `users_registration` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `postings`
---
-
-LOCK TABLES `postings` WRITE;
-/*!40000 ALTER TABLE `postings` DISABLE KEYS */;
-INSERT INTO `postings` VALUES (1,'Dolores St &, 19th St, San Francisco, CA 94114','Air','In Progress','/database/doloresPark.jpg'),(2,'Filbert St & Stockton Street, San Francisco, CA 94133','Water','In Progress','/database/washingtonSquare.jpg'),(3,'1 Telegraph Hill Blvd, San Francisco, CA 94133','Garbage','In Progress','/database/pioneerPark.jpg'),(4,'1705 14th Ave, San Francisco, CA 94122','Hazardous Material','Pending','/database/grandviewPark.jpg'),(5,'Elk St and Chenery Street, San Francisco, CA 94127','Air','Complete','/database/glenCanyonPark.jpg');
-/*!40000 ALTER TABLE `postings` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,15 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-16  1:53:31
-
-
-CREATE TABLE users(
-	id INT NOT NULL AUTO_INCREMENT,
-	username VARCHAR(255) NOT NULL UNIQUE,
-	password VARCHAR(255) NOT NULL,
-	firstname VARCHAR(255) NOT NULL,
-	lastname VARCHAR(255) NOT NULL,
-	create_date DATETIME NOT NULL DEFAULT NOW(),
-	PRIMARY KEY (id)
-);
+-- Dump completed on 2019-07-27 15:11:32
