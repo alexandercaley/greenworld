f3f63a4c059f53100fb28216fafb539ea85c8ef3
Han Huang <47128647+yiyuanh3@users.noreply.github.com>
2019-07-31 02:30:19 -0700

    Added footer

--- application/client/src/components/pages/Category.js
+++ application/client/src/components/pages/Category.js
@@ -1,4 +1,35 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const footerStyle = {
+  backgroundColor: "#184E68",
+  fontSize: "15px",
+  color: "white",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "10px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "50px",
+  width: "100%"
+};
+
+const phantomStyle = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%"
+};
+
+function Footer({ children }) {
+  return (
+    <div>
+      <div style={phantomStyle} />
+      <div style={footerStyle}>{children}</div>
+    </div>
+  );
+}
 
 class Category extends Component {
   constructor() {
@@ -22,30 +53,75 @@ class Category extends Component {
   }
   render() {
     return (
-      <div className="dropdown" style={{ background: "red", width: "150px" }}>
-        <div className="button" onClick={this.showDropdownMenu}>
-          {" "}
-          {"Item Category ^"}
-        </div>
-        {this.state.displayMenu ? (
-          <ul>
-            <li>
-              <a className="active" href="#Plastc">
-                Plastc
+      <body>
+        <div className="dropdown" style={{ background: "red", width: "150px" }}>
+          <div className="button" onClick={this.showDropdownMenu}>
+            {" "}
+            {"Item Category ^"}
+          </div>
+          {this.state.displayMenu ? (
+            <ul>
+              <li>
+                <a className="active" href="#Plastc">
+                  Plastc
               </a>
-            </li>
-            <li>
-              <a href="#Tree Fall">Tree Fall</a>
-            </li>
-            <li>
-              <a href="#Garbage">Garbage </a>
-            </li>
-            <li>
-              <a href="#Water flood">Water flood </a>
-            </li>
-          </ul>
-        ) : null}
-      </div>
+              </li>
+              <li>
+                <a href="#Tree Fall">Tree Fall</a>
+              </li>
+              <li>
+                <a href="#Garbage">Garbage </a>
+              </li>
+              <li>
+                <a href="#Water flood">Water flood </a>
+              </li>
+            </ul>
+          ) : null}
+        </div>
+
+        <div>
+          <Footer>
+            <div class="row">
+              <Link
+                className="nav-link text-white text-lowercase ml-5"
+                to="/home"
+              >
+                Home
+                <i className="fas fa-home" />
+              </Link>
+
+              <Link
+                className="nav-link text-white text-lowercase ml-5"
+                to="/news"
+              >
+                news
+              </Link>
+
+              <Link
+                className="nav-link text-white text-lowercase ml-5"
+                to="/aboutUs"
+              >
+                About Us
+              </Link>
+
+              <Link
+                className="nav-link text-white text-lowercase ml-5"
+                to="/post"
+              >
+                Post
+              </Link>
+
+              <Link
+                className="nav-link text-white text-lowercase ml-5"
+                to="/category"
+              >
+                Category
+              </Link>
+            </div>
+
+          </Footer>
+        </div>
+      </body>
     );
   }
 }
