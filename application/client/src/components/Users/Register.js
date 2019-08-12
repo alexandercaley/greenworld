45ee7bcc397bcb11d23e9a3cbcf8a31558a8d256
Austin Tsang <aytsang14@gmail.com>
2019-08-12 02:13:15 -0700

    fixed bug where after user registers, and tries to click register button again, it wouldnt show up. Now if user clicks register button again after registering, can still register

--- application/client/src/components/Users/Register.js
+++ application/client/src/components/Users/Register.js
@@ -44,7 +44,7 @@ class Register extends Component {
     // split into arrays to guarentee no spacing
     let firstName1 = firstName.split(" ");
     let lastName1 = lastName.split(" ");
-    let email1 = email.split(" ")
+    let email1 = email.split(" ");
     let username1 = username.split(" ");
     let password1 = password.split(" ");
 
@@ -84,10 +84,10 @@ class Register extends Component {
             this.props.history.push(path);
           } else if (message === "USER_ALREADY_EXISTS") {
             console.log("user already exists");
+            this.props.userAlreadyExists();
           } else if (message === "ERR_NO_PASSWORD") {
             console.log("handle no password error");
           }
-          // }
         })
         .catch(err => {
           console.log(err.message);
@@ -106,62 +106,58 @@ class Register extends Component {
     } = this.props;
     return (
       <div>
-        {!isRegistered && (
-          <Form
-            noValidate
-            validated={validated}
-            onSubmit={e => this.handleSubmit(e)}
-            className="RegisterForm"
-          >
-            <Form.Row>
-              <Form.Group as={Col} md="4" controlId="firstName">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="First name"
-                  defaultValue="Mark"
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="lastName">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Last name"
-                  defaultValue="Otto"
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="8" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="text" placeholder="Email" />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="8" controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="User Name" />
-                {USER_ALREADY_EXISTS && (
-                  <p className="errorHandler">User already exists</p>
-                )}
-              </Form.Group>
-              <Form.Group as={Col} md="8" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </Form.Row>
-            {FORM_NOT_FULLFILLED && (
-              <p className="errorHandler">Please fill out form</p>
-            )}
-            {SPACE_IN_USERNAME && (
-              <p className="errorHandler">
-                No spaces are allowed as characters
-              </p>
-            )}
-            <Button type="submit">Register</Button>
-          </Form>
-        )}
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={e => this.handleSubmit(e)}
+          className="RegisterForm"
+        >
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="firstName">
+              <Form.Label>First name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="First name"
+                defaultValue="Mark"
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="lastName">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Last name"
+                defaultValue="Otto"
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="8" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="text" placeholder="Email" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="8" controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="User Name" />
+              {USER_ALREADY_EXISTS && (
+                <p className="errorHandler">User already exists</p>
+              )}
+            </Form.Group>
+            <Form.Group as={Col} md="8" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Form.Row>
+          {FORM_NOT_FULLFILLED && (
+            <p className="errorHandler">Please fill out form</p>
+          )}
+          {SPACE_IN_USERNAME && (
+            <p className="errorHandler">No spaces are allowed as characters</p>
+          )}
+          <Button type="submit">Register</Button>
+        </Form>
         {/* {registered && <Login />} */}
       </div>
     );
