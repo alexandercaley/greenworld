adfbfa8c8569d10441d080349bcaf698e433993d
Austin Tsang <aytsang14@gmail.com>
2019-08-13 22:09:00 -0700

    updated host in ecosystem file

--- application/ecosystem.config.js
+++ application/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-52-53-193-123.us-west-1.compute.amazonaws.com',
+            host: 'ec2-18-144-30-72.us-west-1.compute.amazonaws.com',
             key: '~/.ssh/team6_csc648.pem',
             ref: 'origin/master',
             repo: 'git@github.com:CSC-648-SFSU/csc648-su19-Team06.git',
